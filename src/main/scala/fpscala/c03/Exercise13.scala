422c79765501435d01aea385fbc370e62d4af890
Will Taylor <will.taylor@dealer.com>
2017-01-16 18:15:12 -0500

    oops - this was wrong.  attempting to fix.  had to copy, need to spend time to understand this

--- src/main/scala/fpscala/c03/Exercise13.scala
+++ src/main/scala/fpscala/c03/Exercise13.scala
@@ -1,11 +1,26 @@
 package fpscala.c03
 
-import fpscala.datastructures.{List => FpList}
+import fpscala.datastructures.{Cons, List => FpList, Nil => FpNil}
 
-object Exercise13 extends App {
+object Exercise13 {
 
-  def foldLeft[A, B](as: FpList[A], z: B)(f: (B, A) => B): B = FpList.foldRight(as, z)((a, b) => f(b, a))
+  // TODO understand this :(
+  // copied from https://github.com/fpinscala/fpinscala/blob/master/answerkey/datastructures/13.answer.scala
+  def foldLeft[A, B](as: FpList[A], z: B)(f: (B, A) => B): B = {
 
-  def foldRight[A, B](as: FpList[A], z: B)(f: (A, B) => B): B = Exercise10.foldLeft(as, z)((b, a) => f(a, b))
+    // step by step
+    type IdentB = B => B
+    def identB: IdentB = (b: B) => b
+    def delayedCombiner: (A, IdentB) => IdentB = (a: A, delayFunc: IdentB) => (b: B) => delayFunc(f(b, a))
+    def go: IdentB = FpList.foldRight(as, identB)(delayedCombiner)
+    go(z)
+
+    // one-liner
+    FpList.foldRight(as, (b:B) => b)((a, g) => b => g(f(b, a)))(z)
+  }
+
+  def foldRight[A, B](as: FpList[A], z: B)(f: (A, B) => B): B = {
+    Exercise10.foldLeft(Exercise12.reverse(as), z)((b, a) => f(a, b))
+  }
 
 }
