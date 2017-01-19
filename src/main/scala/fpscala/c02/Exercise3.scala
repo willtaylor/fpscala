11fe3c9be93c2f31d9086f3d7b636d29609c176d
Will Taylor <will.taylor@dealer.com>
2017-01-19 11:46:03 -0500

    not cheating

--- src/main/scala/fpscala/c02/Exercise3.scala
+++ src/main/scala/fpscala/c02/Exercise3.scala
@@ -2,6 +2,6 @@ package fpscala.c02
 
 object Exercise3 {
 
-  def curry[A, B, C](f: (A, B) => C): A => (B => C) = f.curried
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = (a: A) => (b: B) => f(a, b)
 
 }
